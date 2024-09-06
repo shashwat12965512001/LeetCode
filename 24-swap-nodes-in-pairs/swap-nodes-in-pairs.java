bb53ac10af037684c8cee47e05cac04875c8cc19
Shashwat Srivastava <shashwat0805@protonmail.com>
2024-09-06 09:34:59 +0530

    Time: 21 ms (100.00%) | Memory: 41.6 MB (7.04%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -0,0 +1,91 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public static ListNode creaListNode(int val) {
+		ListNode root = new ListNode();
+		root.val = val;
+		root.next = null;
+		return root;
+	}
+
+    public static ListNode insertAtFirst(ListNode root, int val) {
+		ListNode newNode = creaListNode(val);
+		if (root == null) {
+			return newNode;
+		}
+		newNode.next = root;
+		return newNode;
+	}
+
+	public static ListNode insertAtLast(ListNode root, int val) {
+		ListNode newNode = creaListNode(val);
+		if (root == null) {
+			return newNode;
+		}
+		ListNode temp = root;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = newNode;
+		return root;
+	}
+
+    public static ListNode swapNodes(ListNode head) {
+		ListNode first = insertAtFirst(null, head.next.val);
+		first.next = creaListNode(head.val);
+		return first;
+	}
+
+	public static ListNode swapPairs(ListNode head) {
+        if (head == null) {
+			// No Elements
+			return null;
+		}else if (head.next == null) {
+			// Only 1 Element
+			return head;
+		}else if (head.next.next == null) {
+			// Only 2 Elements
+			return swapNodes(head);
+		}
+		int n = 0;
+		ListNode temp = head;
+		while (temp != null) {
+			temp = temp.next;
+			n++;
+		}
+		Stack<Integer> s = new Stack<>();
+		temp = head;
+		int size = (n % 2) == 0 ? n : (n-1);
+		System.out.println("n: "+n+" | size: "+size);
+		ListNode root = null;
+		for (int i = 0; i < size; i++) {
+			System.out.print("(i % 2): "+(i % 2)+" | ");
+			if (i > 0 && (i % 2) == 0) {
+				System.out.print("if | ");
+				while (!s.empty()) {
+					root = insertAtLast(root, s.pop());
+				}
+			}
+			System.out.println("i: "+i+" | temp.val: "+ temp.val);
+			s.push(temp.val);
+			temp = temp.next;
+		}
+        while (!s.empty()) {
+			root = insertAtLast(root, s.pop());
+		}
+		if (n % 2 != 0) {
+			root = insertAtLast(root, temp.val);
+		}
+		return root;
+    }
+
+}
