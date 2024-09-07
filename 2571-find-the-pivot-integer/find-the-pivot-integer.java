50a16fbdf623cf7e4185a94f0ff0b1573a9e2418
Shashwat Srivastava <shashwat0805@protonmail.com>
2024-09-07 14:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (52.83%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,16 +1,5 @@
 class Solution {
     public int pivotInteger(int n) {
-        // int left = 0;
-        // int right = n*(n+1)/2;
-        // for(int k=1;k<=n;k++){
-        //     left += k;
-        //     right -= (k-1);
-        //     if(left == right){
-        //         return k;
-        //     }
-        // }
-        // return -1;
-
         int cummulative_sum = n*(n+1)/2;
         int pivot = (int)Math.sqrt(cummulative_sum);
         return (pivot*pivot == cummulative_sum) ? pivot : -1;
