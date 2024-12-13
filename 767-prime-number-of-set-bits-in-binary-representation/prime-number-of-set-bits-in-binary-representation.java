90e9c0030e052ced24896a8fdb5b8d67e689c9dc
Shashwat Srivastava <shashwat0805@protonmail.com>
2024-12-13 17:06:29 +0530

    Time: 2743 ms (5.05%) | Memory: 45.8 MB (5.86%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -0,0 +1,85 @@
+import java.math.BigInteger;
+class Solution {
+    public static BigInteger decimalToBinary(int num) {
+        BigInteger temp = BigInteger.valueOf(num);
+        BigInteger result = BigInteger.ZERO;
+        BigInteger pow = BigInteger.ONE;
+        BigInteger two = BigInteger.valueOf(2);
+
+        while (temp.compareTo(BigInteger.ZERO) > 0) {
+            result = result.add(temp.mod(two).multiply(pow));
+            temp = temp.divide(two);
+            pow = pow.multiply(BigInteger.TEN);
+        }
+
+        return result;
+    }
+
+    public static int noOfOnes(BigInteger num) {
+        BigInteger temp = num;
+        int count = 0;
+        BigInteger ten = BigInteger.TEN;
+        BigInteger one = BigInteger.ONE;
+    
+        while (temp.compareTo(BigInteger.ZERO) > 0) {
+            if (temp.mod(ten).equals(one)) {
+                count++;
+            }
+            temp = temp.divide(ten);
+        }
+    
+        return count;
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 11) {
+            switch (num) {
+                case 1:
+                    return false;
+                case 2:
+                    return true;
+                case 3:
+                    return true;
+                case 4:
+                    return false;
+                case 5:
+                    return true;
+                case 6:
+                    return false;
+                case 7:
+                    return true;
+                case 8:
+                    return false;
+                case 9:
+                    return false;
+                case 10:
+                    return false;
+                case 11:
+                    return true;
+            }
+        }
+        for (int i = 2; i < num; i++) {
+            if ((num % i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            BigInteger binary = decimalToBinary(i);
+            int ones = noOfOnes(binary);
+            if (isPrime(ones)) {
+                // System.out.print("if | ");
+                count++;
+                System.out.println("i: "+i+" |  binary: "+binary+" | ones: "+ones+" | count: " + count);
+            }
+            // if (isPrime(noOfOnes(decimalToBinary(i)))) {
+            //     count++;
+            // }
+        }
+        return count;
+    }
+}
