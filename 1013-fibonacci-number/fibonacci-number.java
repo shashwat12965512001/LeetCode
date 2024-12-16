579f4686a5c87f8df8172500d0336ddfe6c57658
Shashwat Srivastava <shashwat0805@protonmail.com>
2024-12-16 19:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (73.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,20 @@
 class Solution {
+    public static int fibUtil(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = fibUtil(n-1, dp) + fibUtil(n-2, dp);
+        return dp[n];
+    }
+
     public static int fib(int n) {
         if (n == 0 || n == 1) {
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int dp[] = new int[n+1];
+        return fibUtil(n, dp);
     }
 }
