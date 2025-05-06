7af908c2f7f6cb344e501bb9ba57cbcbf756761e
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-06 18:38:47 +0530

    Time: 1 ms (83.84%) | Memory: 40.7 MB (85.26%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,18 +1,13 @@
-import java.math.BigInteger;
 class Solution {
-    public static int reverse(int x) {
-        boolean isnegative = x < 0;
-        if (Math.abs(x) >= 2147483647 || Math.abs(x) <= -2147483648) {
-            return 0;
+    public int reverse(int x) {
+        double answer = 0;
+        while (x != 0) {
+            answer = (answer * 10) + (x % 10);
+            if (answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) {
+                return 0;
+            }
+            x /= 10;
         }
-        x = Math.abs(x);
-        StringBuilder str = new StringBuilder(String.valueOf(x));
-        str.reverse();
-        BigInteger result = new BigInteger(str.toString());
-        BigInteger test = new BigInteger("2147483647");
-        if (result.compareTo(test) == 0 || result.compareTo(test) == 1) {
-            return 0;
-        }
-        return isnegative ? -Integer.valueOf(str.toString()) : Integer.valueOf(str.toString());
+        return (int) answer;
     }
 }
