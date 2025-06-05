43158062e2c605c735c210b11f12c351f61a1ce5
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-06-05 16:37:20 +0530

    Time: 1 ms (84.68%) | Memory: 42.3 MB (25.65%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,55 +1,23 @@
 class Solution {
-    public static List<Integer> baseCase(int numRows) {
-        List<Integer> li = new ArrayList<>();
-        switch (numRows) {
-            case 0:
-                break;
-            case 1:
-                li.add(1);
-                break;
-            case 2:
-                li.add(1);
-                li.add(1);
-                break;
-            case 3:
-                li.add(1);
-                li.add(2);
-                li.add(1);
-                break;
-        }
-        return li;
-    }
 
     public static List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> li = new ArrayList<>();
-        if (numRows <= 3) {
-            switch (numRows) {
-                case 0:
-                    break;
-                case 1:
-                    list.add(baseCase(1));
-                    break;
-                case 2:
-                    list.add(baseCase(1));
-                    list.add(baseCase(2));
-                    break;
-                case 3:
-                    list.add(baseCase(1));
-                    list.add(baseCase(2));
-                    list.add(baseCase(3));
-                    break;
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> prev = new ArrayList<>();
+            if (i > 2) {
+                prev = result.get(i-2);
             }
-            return list;
+            List<Integer> li = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i-1) {
+                    li.add(1);
+                }
+                else if (i > 2) {
+                    li.add(prev.get(j-1) + prev.get(j));
+                }
+            }
+            result.add(li);
         }
-        List<List<Integer>> last = generate(numRows-1);
-        li.add(1);
-        List<Integer> l = last.get(last.size()-1);
-        for (int i = 1; i < l.size(); i++) {
-            li.add(l.get(i-1) + l.get(i));
-        }
-        li.add(1);
-        last.add(li);
-        return last;
+        return result;
     }
 }
