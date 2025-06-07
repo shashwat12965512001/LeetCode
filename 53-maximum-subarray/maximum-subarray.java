d71c48f99878a7024e61187ccaaabc50039ec471
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-06-07 12:41:56 +0530

    Time: 2 ms (5.52%) | Memory: 56.9 MB (70.57%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,31 @@
+class Solution {
+    int getHighestNegative(int[] nums) {
+        int high = Integer.MIN_VALUE;
+        for (int i : nums) {
+            high = Math.max(high, i);
+        }
+        return high;
+    }
+
+    boolean allNegatives(int[] nums) {
+        for (int i : nums) {
+            if (i >= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int maxSubArray(int[] nums) {
+        if (allNegatives(nums)) {
+            return getHighestNegative(nums);
+        }
+        int maxSum, currSum;
+        maxSum = currSum = 0;
+        for (int i : nums) {
+            currSum = (currSum + i) > 0 ? (currSum + i) : 0;
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
