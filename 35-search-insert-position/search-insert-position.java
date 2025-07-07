cfc0b2523c4e1225a25b78e409d0458865744187
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-07-07 18:21:21 +0530

    Time: 9 ms (0.88%) | Memory: 43.2 MB (28.17%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,41 +1,31 @@
 class Solution {
     public static int searchInsert(int[] nums, int target) {
-        int n = nums.length;
         int start = 0;
-        int end = n-1;
-
-        // Base Cases
-        if (target <= nums[start] || n == start) {
-            return start;
-        }
-        if (target > nums[end]) {
-            return end+1;
-        }
-        if (target == nums[end]) {
-            return end;
-        }
-        if (n == 1) {
-            if (nums[start] >= target) {
-                return start;
-            }
-            return start+1;
-        }
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
+        int n = nums.length;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            System.out.print("nums[" + mid + "]: " + nums[mid] + " | ");
             if (nums[mid] == target) {
                 return mid;
-            }else if (target < nums[mid]) {
-                if (target > nums[mid-1]) {
+            } else if (target > nums[mid]) {
+                if (mid == n - 1) {
+                    return mid + 1;
+                }
+                if (target < nums[mid + 1]) {
+                    return mid + 1;
+                }
+                start = mid + 1;
+            } else {
+                if (mid == 0) {
                     return mid;
                 }
-                end = mid;
-            }else {
-                if (target < nums[mid+1]) {
-                    return mid+1;
+                if (target > nums[mid - 1]) {
+                    return mid;
                 }
-                start = mid+1;
+                end = mid - 1;
             }
+            System.out.println();
         }
         return -1;
     }
