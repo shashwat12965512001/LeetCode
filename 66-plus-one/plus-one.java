8dda148d19002b35cc2a03b24daa9bc6b8f20689
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-07-07 19:27:32 +0530

    Time: 3 ms (3.45%) | Memory: 42.9 MB (9.47%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,68 +1,27 @@
+import java.math.BigInteger;
+
 class Solution {
+    public static BigInteger arrToInt(int[] digits) {
+        BigInteger num = BigInteger.ZERO;
+        for (int digit : digits) {
+            num = num.multiply(BigInteger.TEN).add(BigInteger.valueOf(digit));
+        }
+        return num;
+    }
+
     public static int[] plusOne(int[] digits) {
-        int n = digits.length;
-
-        // Edge cases
-        if (n == 1) {
-            switch (digits[0]) {
-                case 0:
-                    digits[0] = 1;
-                    return digits;
-                case 1:
-                    digits[0] = 2;
-                    return digits;
-                case 2:
-                    digits[0] = 3;
-                    return digits;
-                case 3:
-                    digits[0] = 4;
-                    return digits;
-                case 4:
-                    digits[0] = 5;
-                    return digits;
-                case 5:
-                    digits[0] = 6;
-                    return digits;
-                case 6:
-                    digits[0] = 7;
-                    return digits;
-                case 7:
-                    digits[0] = 8;
-                    return digits;
-                case 8:
-                    digits[0] = 9;
-                    return digits;
-                case 9:
-                    int arr[] = new int[2];
-                    arr[0] = 1;
-                    arr[1] = 0;
-                    return arr;
-            }
+        BigInteger num = arrToInt(digits);
+        ArrayList<BigInteger> li = new ArrayList<>();
+        num = num.add(BigInteger.ONE);
+        while (!num.equals(BigInteger.ZERO)) {
+            li.add(num.mod(BigInteger.TEN));
+            num = num.divide(BigInteger.TEN);
         }
-        
-
-        boolean all9 = true;
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] != 9) {
-                all9 = false;
-            }
+        Collections.reverse(li);
+        int[] result = new int[li.size()];
+        for (int i = 0; i < li.size(); i++) {
+            result[i] = li.get(i).intValue();
         }
-        if (all9) {
-            int arr[] = new int[n+1];
-            arr[0] = 1;
-            return arr;
-        }
-        
-        int i = n-1;
-        int last = digits[i];
-        while (i > 0 && last == 9) {
-            digits[i] = 0;
-            i--;
-            last = digits[i];
-        }
-        if (i >= 0 && i <= n-1) {
-            digits[i] += 1;
-        }
-        return digits;
+        return result;
     }
 }
