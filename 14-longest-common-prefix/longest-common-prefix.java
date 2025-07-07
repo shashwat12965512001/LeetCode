596caa90344c6643099a45b44ef9df5c7ae9332c
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-07-07 14:15:05 +0530

    Time: 3 ms (16.75%) | Memory: 43.1 MB (5.19%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,49 +1,36 @@
 class Solution {
+    public static int getLowest(String[] strs) {
+        int len = Integer.MAX_VALUE;
+        for (int i = 0; i < strs.length; i++) {
+            len = Math.min(len, strs[i].length());
+        }
+        return len;
+    }
+
+    public static boolean allSame(String[] strs) {
+        for (int i = 0; i < strs.length - 1; i++) {
+            if (!strs[i].equals(strs[i + 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static String longestCommonPrefix(String[] strs) {
-		if (strs.length == 0) {
-			return "";
-		}
-		HashMap<Integer, String> map = new HashMap<>();
-		StringBuilder prefix = new StringBuilder();
-		int index = 0;
-		int minlen = Integer.MAX_VALUE;
-		for (int i = 0; i < strs.length; i++) {
-			minlen = Math.min(minlen, strs[i].length());
-		}
-		if (minlen == Integer.MAX_VALUE || minlen == 0) {
-			return "";
-		}
-		map.put(index, String.valueOf(strs[0].charAt(0)));
-		while (index < minlen) {
-			boolean isbreak = false;
-			int count = 0;
-			for (int i = 0; i < strs.length; i++) {
-				char ch = strs[i].charAt(index);
-				String val = map.get(index);
-				if (val == null) {
-					count++;
-					map.put(index, String.valueOf(ch));
-				}else if (!val.equals(String.valueOf(ch))) {
-					isbreak = true;
-					break;
-				}else{
-					count++;
-					map.put(index, String.valueOf(ch));
-				}
-			}
-			if (!isbreak) {
-				prefix.append(map.get(index));
-			}else if (count == strs.length) {
-				prefix.append(map.get(index));
-				count = 0;
-			}else{
-				break;
-			}
-			if (index == 0 && prefix.toString().isEmpty()) {
-				return "";
-			}
-			index++;
-		}
-		return prefix.toString();
+        String prefix = "";
+        int n = strs.length;
+        int len = getLowest(strs);
+        for (int i = 0; i < len; i++) {
+            String[] prefixArr = new String[n];
+            for (int j = 0; j < n; j++) {
+                String single = strs[j];
+                prefixArr[j] = String.valueOf(single.charAt(i));
+            }
+            if (!allSame(prefixArr)) {
+                return prefix;
+            }
+            prefix += prefixArr[0];
+        }
+        return prefix;
     }
 }
